f86c2c2cccec47f42e1c7ccb0e2e1e19c299988b
LAPTOP-LDL7JQH2\Legion <yahianagib112@gmail.com>
2025-11-18 20:30:24 +0200

    testing

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,16 @@
+import utils
+import unittest
+
+class TestAccessNestedMap(unittest.TestCase):
+    
+    def test_access_nested_map(self):
+
+        nested_map={"a": {"b": 2}, "b" : {"c":5 , "d":77}}
+        path=("b", "d")
+
+        self.assertEqual(utils.access_nested_map(nested_map,path),77)
+
+        nested_map={"a": {"b": 2}}
+        path=("a", "b")
+
+        self.assertEqual(utils.access_nested_map(nested_map,path),2)
