dcacfa7fe710eaeb883b7969333173aa3c8793ea
LAPTOP-LDL7JQH2\Legion <yahianagib112@gmail.com>
2025-12-07 02:36:01 +0200

    f

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,9 +1,10 @@
 # chats/urls.py
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from . import views
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
+
 router.register(r'users', views.UserViewSet)
 router.register(r'conversations', views.ConversationViewSet, basename='conversation')
 router.register(r'messages', views.MessageViewSet, basename='message')
